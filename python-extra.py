bb3a180ca5076297a7c088f00abb741c1935ddaf
BM Roberts <b.roberts@uq.edu.au>
2024-02-14 15:29:43 +1000

    Extends python example

---
+++ python-extra.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import numpy as np
+from matplotlib import pyplot as plt
+
+filename = "data-csv-labels.dat"
+
+xy_data = np.genfromtxt(filename, delimiter=",", names=True, skip_header=3)
+
+# We can get the column names if we set 'names=True':
+column_names = xy_data.dtype.names
+print(column_names)
+xlabel = column_names[0]
+ylabel = column_names[1]
+
+# With "names", can address the columns by their name (string)
+x_data = xy_data['xlabel']
+y_data = xy_data['ylabel']
+
+# Example: plot the data
+plt.title('Example: plot of function')
+plt.xlabel(column_names[0] + ' (units)')
+plt.ylabel('function (units)')
+plt.plot(x_data, y_data, 'r-', label = column_names[1])
+plt.plot(x_data, y_data*(1.25), 'k--', label = "error")
+plt.plot(x_data, y_data*(0.75), 'k--')
+plt.legend()
+
+plt.show()
--- python.py
+++ python.py
@@ -12,26 +12,32 @@ contents = file1.read()
 
 # -------------------
 
+# We usually load the data from a text file directly into an array
+
 # Read file as 2D (x,y) array - sometimes useful, sometimes not
-xy_data = np.loadtxt(filename)
-
-# We can read each column one-by-one
-x_data = np.loadtxt(filename, usecols=0)
-y_data = np.loadtxt(filename, usecols=1)
-
-# https://numpy.org/doc/stable/reference/generated/numpy.loadtxt.html
-
-# The 'genfromtxt' function is similar, but gives some mor fine-tuned controls
-# https://numpy.org/doc/stable/reference/generated/numpy.genfromtxt.html
-
-# note: for *very* large input files, this might be too slow
-# since it reads through the file twice.
+xy_data = np.genfromtxt(filename)
 
 # We can also extract the x and y parts of the xy array:
 # If we have huge data files, we want to avoid needless copies,
 # but this also works:
 x_data_2 = xy_data[:,0]
 y_data_2 = xy_data[:,1]
+
+# We can also read each column one-by-one
+x_data = np.genfromtxt(filename, usecols=0)
+y_data = np.genfromtxt(filename, usecols=1)
+
+# For different delimeters, use:
+# xy_data = np.loadtxt(filename, delimiter=",")
+
+# By default, pythong considers '#' to be a comment
+# We can change this, using 'comments' field:
+# xy_data = np.loadtxt(filename, delimiter=",", comments='!')
+
+# Full documentation:
+# https://numpy.org/doc/stable/reference/generated/numpy.genfromtxt.html
+# The 'loadtxt' function is similar, but is a little less advanced
+# https://numpy.org/doc/stable/reference/generated/numpy.loadtxt.html
 
 # Example: plot the data
 plt.title('Example: plot of function')
