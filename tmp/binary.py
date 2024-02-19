c6cca17a11e7cfbcc82bc40139c44e0a07087f49
BM Roberts <b.roberts@uq.edu.au>
2024-02-19 13:41:36 +1000

    Adds raw binary example

---
+++ tmp/binary.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import numpy as np
+
+filename = "data.dat"
+
+txt_filename = "data.text"
+bin_filename = "data.binary"
+bin_filename2 = "data.binary2"
+
+# As a start, read in the same example data from from before
+xy_data = np.genfromtxt(filename)
+
+# Just to make the numbers not round, multiply by pi:
+xy_data *= np.pi
+
+
+# Write out to text file, keeping full precission:
+np.savetxt(txt_filename, xy_data)
+
+# The above is equivilant to:
+# with open(txt_filename, "wt") as file:
+#     for x, y in xy_data:
+#         print("{:.18e} {:.18e}".format(x, y), file=file)
+
+
+# Now, write to file in binar format:
+xy_data.tofile(bin_filename + "2")
+
+# The above is equivilant to:
+with open(bin_filename2, "wb") as file:
+    for x, y in xy_data:
+        file.write(x)
+        file.write(y)
+
+
+# Look at the file sizes of the text vs. binary file:
+# The binary file should be ~3x smaller.
+# This is because we only wrote the exact required bytes to the file.
+
+
+xy_data_in = np.fromfile(bin_filename)
+xy_data_in2 = np.fromfile(bin_filename2)
+
+# We can check these are the same:
+print(xy_data_in - xy_data_in2)
+
+# Note: this returned us 1D data, when we wrote a 2D array
+# None of the shape information was written to the file
+
+num_cols = 2
+num_rows = int(len(xy_data_in) / num_cols)
+
+xy_data_in = xy_data_in.reshape(num_rows, num_cols)
+
+# Now, we can check that this has the right size/shape, and is the same as the
+# original data:
+print(xy_data - xy_data_in)
---
+++ tmp/pickle-example.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import numpy as np
+import pickle as pkl
+
+# pip install pickle5
+
+filename = "data.dat"
+
+pkl_filename = "data.pkl"
+
+# As a start, read in the same example data from from before
+xy_data = np.genfromtxt(filename)
+
+# Just to make the numbers not round, multiply by pi:
+xy_data *= np.pi
+
+with open(pkl_filename, "wb") as file:
+    pkl.dump(xy_data, file)
+
+# The "pickle" file is slightly larger than the raw binary.
+# But it knows about python data structures.
+# If you're exclusively inside python, it's a great choice, but not portable.
+
+
+# Extract the "pickle" simply
+with open(pkl_filename, "rb") as file:
+    xy_data_in = pkl.load(file)
+
+print(xy_data_in - xy_data)
