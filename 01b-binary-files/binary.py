4b6cdb0d51ee20fe6cbafeb9c22dfdf46ace4a13
BM Roberts <b.roberts@uq.edu.au>
2024-02-20 11:44:33 +1000

    Minor: update comments

--- 01b-binary-files/binary.py
+++ 01b-binary-files/binary.py
@@ -13,13 +13,13 @@ xy_data = np.genfromtxt(filename)
 # Write out to text file, keeping full precission:
 np.savetxt(txt_filename, xy_data)
 
-# The above is equivilant to:
-# with open(txt_filename, "wt") as file:
+# # The above is equivilant to:
+# with open(txt_filename, "wt") as out_file:
 #     for x, y in xy_data:
-#         print("{:.18e} {:.18e}".format(x, y), file=file)
+#         print("{:.18e} {:.18e}".format(x, y), file=out_file)
 
 
-# Now, write to file in binar format:
+# Now, write to file in raw binary format:
 xy_data.tofile(bin_filename)
 
 # The above is equivilant to:
@@ -32,19 +32,19 @@ with open(bin_filename2, "wb") as file:
 # The binary file should be ~3x smaller.
 # This is because we only wrote the exact required bytes to the file.
 
-xy_data_in = np.fromfile(bin_filename)
-xy_data_in2 = np.fromfile(bin_filename2)
+data_in = np.fromfile(bin_filename)
+data_in2 = np.fromfile(bin_filename2)
 
 # We can check these are the same:
-print(xy_data_in - xy_data_in2)
+print(data_in - data_in2)
 
 # Note: this returned us 1D data, when we wrote a 2D array
 # None of the shape information was written to the file
 
 num_cols = 2
-num_rows = int(len(xy_data_in) / num_cols)
+num_rows = int(len(data_in) / num_cols)
 
-xy_data_in = xy_data_in.reshape(num_rows, num_cols)
+xy_data_in = data_in.reshape(num_rows, num_cols)
 
 # Now, we can check that this has the right size/shape, and is the same as the
 # original data:
